db977aaf6d73ba31d9bae93124cd6b7c59bd4843
Rob Ruana <rob@robruana.com>
2018-05-02 15:21:51 -0400

    Adds ability to limit or deny services and applications.
    The "limit" and "deny" parameters have been added to both the services and
    applications sections. Setting "limit: True" will use the "ufw limit" command
    instead of "ufw allow". Likewise, setting "deny: True" will use the "ufw deny"
    command.

--- test/integration/ufw/controls/ufw.rb
+++ test/integration/ufw/controls/ufw.rb
@@ -25,7 +25,47 @@ describe command('ufw status verbose | grep Logging') do
   its('stdout') { should match /low/ }
 end
 
-describe command('ufw status | grep 22/tcp') do
+describe command('ufw status | grep MySQL') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match /ALLOW/ }
+end
+
+describe command('ufw status | grep Postgresql') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /LIMIT/ }
+end
+
+describe command('ufw status | grep SSH223') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /DENY/ }
+end
+
+describe command('ufw status | grep 10.0.0.0') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /DENY/ }
+end
+
+describe command('ufw status | grep 22/tcp') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /LIMIT/ }
+end
+
+describe command('ufw status | grep 80/tcp') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /DENY/ }
+end
+
+describe command('ufw status | grep 443/tcp') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /ALLOW/ }
+end
+
+describe command('ufw status | grep 10.0.0.1') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /DENY/ }
+end
+
+describe command('ufw status | grep 10.0.0.2') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match /DENY/ }
 end
